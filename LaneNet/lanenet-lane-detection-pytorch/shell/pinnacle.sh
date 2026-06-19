34d9e30ab50748d1f08b1bfcb4421ecb4cb7cf82
Aman Nindra <amannindra@gmail.com>
2026-06-19 05:19:46 -0700

    Add initial job script for LaneNet training with SLURM configuration

---
+++ LaneNet/lanenet-lane-detection-pytorch/shell/pinnacle.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=lanenet_train
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:h100:1
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+
+conda activate LaneNet310
+
+python /home/anindra/data/Autonomous-Bicycle/LaneNet/lanenet-lane-detection-pytorch/train.py \
+    --dataset /home/anindra/data/TUSimple/train_set/training \
+    --epochs 25 \
+    --bs 12 \
+    --save ./log
