2f3cea0a4ec670f81c33d293b01115af0c7ead9b
Aman Nindra <amannindra@gmail.com>
2026-08-24 14:41:35 -0700

    ads

--- ros2_ws/src/center_lane/center_lane/center.py
+++ ros2_ws/src/center_lane/center_lane/center.py
@@ -11,8 +11,41 @@ import time
 class CenterNode(Node):
     def __init__(self):
         super().__init__('center_node')
-        self.get_split_left = self.create_subscription(Float32MultiArray, "/laneatt/left_lane", self.lane_callback,1)
-        self.get_split_right = self.create_subscription(Float32MultiArray, "/laneatt/right_lane", self.lane_callback,  1)
+        self.get_split_left = self.create_subscription(Float32MultiArray, "/laneatt/left_lane", self.left_lane_callback,1)
+        self.get_split_right = self.create_subscription(Float32MultiArray, "/laneatt/right_lane", self.right_lane_callback, 1)
+        
+        self.left_lane_pts = None
+        self.right_lane_pts = None
+    
+    def left_lane_callback(self, msg):
+        data = np.array(msg.data, dtype=np.float32)
+        if len(data) > 0:
+            # Reshape flat [x, y, x, y...] array back into (N, 2) coordinates
+            self.left_lane_pts = data.reshape(-1, 2)
+        else:
+            self.left_lane_pts = None
+            
+            
+    def right_lane_callback(self, msg):
+        data = np.array(msg.data, dtype=np.float32)
+        if len(data) > 0:
+            self.right_lane_pts = data.reshape(-1, 2)
+        else:
+            self.right_lane_pts = None
+        
+        self.get_center()
+    
+    def get_center(self):
+        if self.right_lane_pts and self.left_lane_pts:
+            self.get_logger().info("Both Found")
+        elif self.right_lane_pts == None:
+            self.get_logger().info("Right Lane Not Found")
+        else:
+            self.get_logger().info("Left Lane Not Found")
+            
+            
+            
+    
         
     def lane_callback(self, msg):
         self.get_logger().info(f'Split_left: Type {type(self.get_split_left)}')
